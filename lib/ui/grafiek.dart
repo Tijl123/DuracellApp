27634c3508fa7f55c286e90ab6e95c93a89ab88a
agit basar <agitbasar@yahoo.be>
2020-02-13 11:16:54 +0100

    knoppen grafiek

--- lib/ui/grafiek.dart
+++ lib/ui/grafiek.dart
@@ -4,7 +4,6 @@ import 'package:fl_animated_linechart/fl_animated_linechart.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-
 class Grafiek extends StatefulWidget {
   Grafiek({Key key, this.title}) : super(key: key);
 
@@ -28,61 +27,67 @@ class _Grafiek extends State<Grafiek> {
       ),
       body: FutureBuilder<List<LogModel>>(
         future: DBProvider.db.getAllLogs(),
-        builder: (BuildContext context, AsyncSnapshot<List<LogModel>> snapshot) {
+        builder:
+            (BuildContext context, AsyncSnapshot<List<LogModel>> snapshot) {
           if (snapshot.hasData) {
             Map<DateTime, double> line1 = {};
             Map<DateTime, double> line2 = {};
             Map<DateTime, double> line3 = {};
             Map<DateTime, double> line4 = {};
             snapshot.data.forEach((log) {
-              var dateTime = new DateFormat("dd/MM/yyyy HH:mm:ss").parse(log.datum);
-                switch (log.sensor) {
-                  case "sensor1":
-                    {
-                      line1[dateTime] = double.parse(log.waarde);
-                    }
-                    break;
-                  case "sensor2":
-                    {
-                      line2[dateTime] = double.parse(log.waarde);
-                    }
-                    break;
-                  case "sensor3":
-                    {
-                      line3[dateTime] = double.parse(log.waarde);
-                    }
-                    break;
-                  case "sensor4":
-                    {
-                      line4[dateTime] = double.parse(log.waarde);
-                    }
-                    break;
-                }
+              var dateTime =
+                  new DateFormat("dd/MM/yyyy HH:mm:ss").parse(log.datum);
+              switch (log.sensor) {
+                case "sensor1":
+                  {
+                    line1[dateTime] = double.parse(log.waarde);
+                  }
+                  break;
+                case "sensor2":
+                  {
+                    line2[dateTime] = double.parse(log.waarde);
+                  }
+                  break;
+                case "sensor3":
+                  {
+                    line3[dateTime] = double.parse(log.waarde);
+                  }
+                  break;
+                case "sensor4":
+                  {
+                    line4[dateTime] = double.parse(log.waarde);
+                  }
+                  break;
+              }
             });
 
             if (line1.isEmpty) {
               line1[DateTime.now()] = 0.0;
             }
-            if(line2.isEmpty){
+            if (line2.isEmpty) {
               line2[DateTime.now()] = 0.0;
             }
-            if(line3.isEmpty){
+            if (line3.isEmpty) {
               line3[DateTime.now()] = 0.0;
             }
-            if(line4.isEmpty){
+            if (line4.isEmpty) {
               line4[DateTime.now()] = 0.0;
             }
 
             LineChart chart;
 
             if (chartIndex == 0) {
-              chart = LineChart.fromDateTimeMaps([line1], [Colors.red.shade900], ['']);
+              chart = LineChart.fromDateTimeMaps(
+                  [line1], [Colors.red.shade900], ['']);
             } else if (chartIndex == 1) {
-              chart = LineChart.fromDateTimeMaps([line2], [Colors.green.shade900], ['']);
+              chart = LineChart.fromDateTimeMaps(
+                  [line2], [Colors.green.shade900], ['']);
             } else if (chartIndex == 2) {
-              chart = LineChart.fromDateTimeMaps([line3], [Colors.yellow.shade900], ['']);
+              chart = LineChart.fromDateTimeMaps(
+                  [line3], [Colors.yellow.shade900], ['']);
             } else {
-              chart = LineChart.fromDateTimeMaps([line4], [Colors.blue.shade900], ['']);
+              chart = LineChart.fromDateTimeMaps(
+                  [line4], [Colors.blue.shade900], ['']);
             }
             return Container(
               child: Column(
@@ -90,34 +95,8 @@ class _Grafiek extends State<Grafiek> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    DropdownButton<String>(
-                    value: dropdownValue,
-                    icon: Icon(Icons.arrow_downward),
-                    iconSize: 24,
-                    elevation: 16,
-                    style: TextStyle(
-                        color: Colors.black
-                    ),
-                    underline: Container(
-                      height: 2,
-                      color: Colors.brown.shade900,
-                    ),
-                    onChanged: (String newValue) {
-                      setState(() {
-                        dropdownValue = newValue;
-                      });
-                    },
-                    items: <String>['One', 'Two', 'Free', 'Four']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    })
-                        .toList(),
-                  ),
                     Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.all(4.0),
                       child: Row(
                         mainAxisSize: MainAxisSize.max,
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -125,12 +104,14 @@ class _Grafiek extends State<Grafiek> {
                           FlatButton(
                             shape: RoundedRectangleBorder(
                                 side: BorderSide(color: Colors.black45),
-                                borderRadius: BorderRadius.all(Radius.circular(3))),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(3))),
                             child: Text(
                               'Sensor 1',
                               style: TextStyle(
-                                  color:
-                                  chartIndex == 0 ? Colors.black : Colors.black12),
+                                  color: chartIndex == 0
+                                      ? Colors.black
+                                      : Colors.black12),
                             ),
                             onPressed: () {
                               setState(() {
@@ -141,7 +122,8 @@ class _Grafiek extends State<Grafiek> {
                           FlatButton(
                             shape: RoundedRectangleBorder(
                                 side: BorderSide(color: Colors.black45),
-                                borderRadius: BorderRadius.all(Radius.circular(3))),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(3))),
                             child: Text('Sensor 2',
                                 style: TextStyle(
                                     color: chartIndex == 1
@@ -153,10 +135,20 @@ class _Grafiek extends State<Grafiek> {
                               });
                             },
                           ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
                           FlatButton(
                             shape: RoundedRectangleBorder(
                                 side: BorderSide(color: Colors.black45),
-                                borderRadius: BorderRadius.all(Radius.circular(3))),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(3))),
                             child: Text('Sensor 3',
                                 style: TextStyle(
                                     color: chartIndex == 2
@@ -171,7 +163,8 @@ class _Grafiek extends State<Grafiek> {
                           FlatButton(
                             shape: RoundedRectangleBorder(
                                 side: BorderSide(color: Colors.black45),
-                                borderRadius: BorderRadius.all(Radius.circular(3))),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(3))),
                             child: Text('Sensor 4',
                                 style: TextStyle(
                                     color: chartIndex == 3
@@ -188,12 +181,12 @@ class _Grafiek extends State<Grafiek> {
                     ),
                     Expanded(
                         child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: AnimatedLineChart(
-                            chart,
-                            key: UniqueKey(),
-                          ), //Unique key to force animations
-                        )),
+                      padding: const EdgeInsets.all(8.0),
+                      child: AnimatedLineChart(
+                        chart,
+                        key: UniqueKey(),
+                      ), //Unique key to force animations
+                    )),
                     SizedBox(width: 200, height: 50, child: Text('')),
                   ]),
             );
