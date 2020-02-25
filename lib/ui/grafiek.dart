47c7595cb2f6642a2a65632a31b359614c194dad
agit basar <agitbasar@yahoo.be>
2020-02-25 10:24:06 +0100

    push

--- lib/ui/grafiek.dart
+++ lib/ui/grafiek.dart
@@ -1,8 +1,10 @@
 import 'package:duracellapp/Database.dart';
 import 'package:duracellapp/LogModel.dart';
+import 'package:duracellapp/SensorModel.dart';
 import 'package:fl_animated_linechart/fl_animated_linechart.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class Grafiek extends StatefulWidget {
   Grafiek({Key key, this.title}) : super(key: key);
@@ -14,7 +16,12 @@ class Grafiek extends StatefulWidget {
 }
 
 class _Grafiek extends State<Grafiek> {
+  Future<List<LogModel>> logs;
   int chartIndex = 0;
+  int b1 = 0;
+  int b2 = 0;
+  int b3 = 0;
+  int b4 = 0;
   String dropdownValue = 'One';
 
   @override
@@ -93,6 +100,7 @@ class _Grafiek extends State<Grafiek> {
               chart = LineChart.fromDateTimeMaps(
                   [line4], [Colors.blue.shade900], ['']);
             }
+
             return Container(
               child: Column(
                   mainAxisSize: MainAxisSize.max,
@@ -189,7 +197,7 @@ class _Grafiek extends State<Grafiek> {
                       child: AnimatedLineChart(
                         chart,
                         key: UniqueKey(),
-                      ), //Unique key to force animations
+                      ),//Unique key to force animations
                     )),
                     SizedBox(width: 200, height: 50, child: Text('')),
                   ]),
@@ -201,4 +209,99 @@ class _Grafiek extends State<Grafiek> {
       ),
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Grafiek"),
+        centerTitle: true,
+        backgroundColor: Colors.brown.shade600,
+      ),
+      body: FutureBuilder<List<LogModel>>(
+        future: DBProvider.db.getAllLogs(),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<LogModel>> snapshot) {
+          if (snapshot.hasData) {
+            snapshot.data.forEach((log) {
+
+
+              //data sensoren indelen naar de juiste grafieken
+              switch (log.sensor) {
+                case "sensor1":
+                  {
+                    b1 = b1 + 1;
+                  }
+                  break;
+                case "sensor2":
+                  {
+                    b2 = b2 + 1;
+                  }
+                  break;
+                case "sensor3":
+                  {
+                    b3 = b3 + 1;
+                  }
+                  break;
+                case "sensor4":
+                  {
+                    b4 = b4 + 1;
+                  }
+                  break;
+              }
+            });
+
+            var data = [
+              ClicksPerYear('Sensor1', b1, Colors.blue.shade900),
+              ClicksPerYear('Sensor2', b2, Colors.blue.shade900),
+              ClicksPerYear('Sensor3', b3, Colors.blue.shade900),
+              ClicksPerYear('Sensor4', b4, Colors.blue.shade900),
+            ];
+
+            var series = [
+              charts.Series(
+                domainFn: (ClicksPerYear clickData, _) => clickData.year,
+                measureFn: (ClicksPerYear clickData, _) => clickData.clicks,
+                colorFn: (ClicksPerYear clickData, _) => clickData.color,
+                id: 'Clicks',
+                data: data,
+              ),
+            ];
+
+            var chart = charts.BarChart(
+              series,
+              animate: true,
+            );
+
+            var chartWidget = Padding(
+              padding: EdgeInsets.all(32.0),
+              child: SizedBox(
+                height: 200.0,
+                child: chart,
+              ),
+            );
+
+            return Container(
+              child: Column(
+                children: <Widget>[
+                chartWidget,
+              ],
+            ));
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
+
+class ClicksPerYear {
+  final String year;
+  final int clicks;
+  final charts.Color color;
+
+  ClicksPerYear(this.year, this.clicks, Color color)
+      : this.color = charts.Color(
+      r: color.red, g: color.green, b: color.blue, a: color.alpha);
 }
